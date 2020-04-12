d757b4b6022538f2c3bc966095567e6dd072e727
Socogo <38353197+SocogoRee@users.noreply.github.com>
2020-04-12 12:20:52 +0800

    tigao

---
+++ src/views/Cinema/index.vue
@@ -0,0 +1,24 @@
+<template>
+	<div id="main">
+		<Header title="哈哈影院"/>
+		<TabBar />		
+	</div>
+</template>
+
+<script>
+
+import Header from '@/components/Header';
+import TabBar from '@/components/TabBar';
+export default {
+	name : 'Cinema',
+	components : {
+		Header,
+		TabBar
+	}
+
+}
+</script>
+
+<style>
+	
+</style>
